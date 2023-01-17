e61b2ff537da1434947ecf306608d99e44de1ec7
Rosie Gardner <rosiegardner78@gmail.com>
2023-01-17 10:30:01 -0800

    update: import HeaderSocial's component  // style: add styles for media queries in header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/rosie.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -13,6 +14,7 @@ const Header = () => {
           Fullstack Developer
         </h5>
         <CTA />
+        <HeaderSocials />
 
         <div className='me'>
           <img src={ME} alt="" />
