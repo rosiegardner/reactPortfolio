3d97f22ee988558febb2fe6af7d29cf6cd3991b8
Rosie Gardner <rosiegardner78@gmail.com>
2023-01-17 10:58:12 -0800

    update: update section tag to have 100vh in index.css // update: replace div tag with section tag in all components // add: add icons to Nav.jsx component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeAlt, BiUser, BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {GiEvilBook} from 'react-icons/gi'
+// import {RiServiceFill} from 'react-icons/ri'
+// import {AiOutlineMessage} from 'react-icons/ai'
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><BiHomeAlt /> Home</a>
+      <a href="#about"><BiUser /> About</a>
+      <a href="#experiences"><BiBook /> Experience</a>
+      <a href="#portfolio"><GiEvilBook /> Portfolio</a>
+      <a href="#contact"><BiMessageSquareDetail /> Contact</a>
+      
+    </nav>
   )
 }
 
