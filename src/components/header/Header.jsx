a78564c154e7bff5d0cfb8956740fa778e31007a
Rosie Gardner <rosiegardner78@gmail.com>
2023-01-16 17:53:04 -0800

    update: css variables in index.css // update: png img in Header.jsx // feature: add CTA.jsx 'call to action' file for contact.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/rosie.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hi, I'm</h5>
+        <h1>Rosie Gardner!</h1>
+        <h5 className="text-light">
+          Fullstack Developer
+        </h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt="" />
+        </div>
+
+        <a href='#contact' className='scroll-down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
