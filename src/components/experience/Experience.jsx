b75b7d1e6b52e56d4181539bff41ad7cc8d3c7b1
Rosie Gardner <rosiegardner78@gmail.com>
2023-01-18 09:53:54 -0800

    feature: add content to Experience.jsx component with react-icons  // style: add styling in experience.css folder for Experience.jsx file Component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import './experience.css'
+import {BsHandThumbsUp} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+              </div>           
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>React</h4>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>Ruby on Rails</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>PostgreSQL</h4>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsHandThumbsUp className='experience__details-icon'/>
+              <div>
+                <h4>Firebase</h4>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
